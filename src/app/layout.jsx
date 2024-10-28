b50ce59ca7ae028aa12e6942dc17888766c4d2f7
jidandev <jidangamer7@gmail.com>
2024-10-28 02:39:18 +0000

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -57,7 +57,7 @@ export default function RootLayout({ children }) {
 
       if (!mounted) return null; // hindari flickering saat rendering awal
   return (
-    <html lang="en" >
+    <html lang="en" class="dark" >
       <head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
